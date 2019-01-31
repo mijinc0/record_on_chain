ae7f2957d5045a07e17d3248157b19a802235567
MIJ <mijinco1024+git@gmail.com>
2019-01-31 14:54:14 +0900

    modify config, keyfile

--- lib/record_on_chain/config.rb
+++ lib/record_on_chain/config.rb
@@ -1,28 +1,12 @@
-require "yaml"
-require_relative "./utils"
+require_relative "./datafile_base"
 
 module RecordOnChain
   class Config
-    @@FILENAME = "config.yml".freeze
-
-    def self.filename
-      return @@FILENAME
-    end
-
-    def self.create( dir_path, default_values={} )
-      yml_str = YAML.dump( default_values )
-      File.open( "#{dir_path}/#{@@FILENAME}", "w" ){ |f| f.write( yml_str ) }
-    end
-
-    def self.load( dir_path )
-      map_data = YAML.load_file( "#{dir_path}/#{@@FILENAME}" )
-      return Config.new( map_data )
-    end
-
-    attr_reader :nem_config
-
-    def initialize( map_data )
-      @nem_config = Utils.symbolize_hashkeys_rf( map_data )[:nem_config] || {}
-    end
+    extend DatafileBase
+    define_datafile_class(
+      :keyfile_path,
+      :recipient,
+      :nodes,
+    )
   end
 end
